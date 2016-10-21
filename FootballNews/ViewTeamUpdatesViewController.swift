6b0b37c677e559f7cb11570c6569acbbcd1828d4
Kanti Krishnan <kantikrishnan@kantis-mac-mini.local>
2016-10-20 20:16:11 -0700

    search controller fixed

--- FootballNews/ViewTeamUpdatesViewController.swift
+++ FootballNews/ViewTeamUpdatesViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import CoreData
 
-class ViewTeamUpdatesTeamsViewController: UITableViewController,UISearchResultsUpdating {
+class ViewTeamUpdatesTeamsViewController: UITableViewController,UISearchResultsUpdating
+{
     @available(iOS 8.0, *)
     public func updateSearchResults(for searchController: UISearchController) {
+        //
         filterContentForSearchText(searchText: searchController.searchBar.text!)
     }
 
@@ -43,8 +45,10 @@ class ViewTeamUpdatesTeamsViewController: UITableViewController,UISearchResultsU
 
     func filterContentForSearchText(searchText:String,scope:String="All")
     {
+        print(allTeams.count)
         filteredTeams = allTeams.filter{team in
             return team.teamName.lowercased().contains(searchText.lowercased())}
+        print(filteredTeams.count)
         tableView.reloadData()
     }
     
@@ -74,19 +78,36 @@ class ViewTeamUpdatesTeamsViewController: UITableViewController,UISearchResultsU
             }
             return matchingEntities.count
         }
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = (tableView.dequeueReusableCellWithIdentifier("CellIdentifier"))! as UITableViewCell
         let teams : [Team] = Team().getStoredObjects("")
         let cell = UITableViewCell()
-        
         // Configure the cell...
         cell.textLabel?.text="No Teams Found..Please add few.."
-        if(!teams.isEmpty && teams.count>0)
-        {
-            cell.textLabel?.text = teams[(indexPath as NSIndexPath).item].teamName
+        
+        var t: Team
+        if searchController.isActive && searchController.searchBar.text != "" {
+            if(!filteredTeams.isEmpty && filteredTeams.count>0)
+            {
+                t = filteredTeams[indexPath.row]
+                cell.textLabel?.text=t.teamName
+                cell.layer.borderWidth = 2.0
+                cell.layer.borderColor = UIColor.cyan.cgColor
+            }
         }
+        else
+        {
+            if(!teams.isEmpty && teams.count>0)
+            {
+                cell.textLabel?.text = teams[(indexPath as NSIndexPath).item].teamName
+            }
+
+        }
+        cell.backgroundColor=UIColor.black
+        cell.textLabel?.textColor=UIColor.white
         return cell
     }
     
@@ -123,8 +144,3 @@ class ViewTeamUpdatesTeamsViewController: UITableViewController,UISearchResultsU
         }
     }
 }
-
-
-
-
-    
