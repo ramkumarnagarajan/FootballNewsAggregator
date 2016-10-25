74eb2f02db9a4ec04b11573de7339bbfa35bc22f
Kanti Krishnan <kantikrishnan@kantis-mac-mini.attlocal.net>
2016-10-25 00:15:31 -0700

    DeveloperCredits VC Added and linked to the Slide out menu

--- FootballNews/SetupViewController.swift
+++ FootballNews/SetupViewController.swift
@@ -12,30 +12,10 @@ class SetupViewController : UIViewController
 {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        btnHome.isEnabled=false
-        btnHome.isHidden = true
-        
-        vwBackgroundView.layer.borderColor = UIColor.blue.cgColor
-        vwBackgroundView.layer.borderWidth = 1
-        let viewBackGroundColor = UIColor(red:36/256,green:36/256,blue:36/256,alpha:1)
-        self.view?.backgroundColor = viewBackGroundColor
-    }
-    
-    @IBOutlet weak var vwBackgroundView: UIView!
-    @IBOutlet weak var btnHome: UIButton!
+}
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    
-    @IBAction func gotoManageTeams(_ sender: AnyObject) {
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        var tlVC = ManageTeamsViewController()
-        tlVC = storyBoard.instantiateViewController(withIdentifier: "ManageTeamsViewController") as! ManageTeamsViewController
-        //tlVC.navigationItem.setHidesBackButton(true, animated: true)
-        self.navigationController?.pushViewController(tlVC, animated: true)
     }
 }
